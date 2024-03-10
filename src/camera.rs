0cbec25467b48fa85f8f7a20848c8ee48f58c093
Ambareesh Shyam Sundar <asundar44@gatech.edu>
2024-03-10 05:22:29 -0400

    flashing light when pegs collide

--- src/camera.rs
+++ src/camera.rs
@@ -1,4 +1,5 @@
 use bevy::{app::AppExit, prelude::*};
+use bevy::core_pipeline::{tonemapping::Tonemapping, bloom::BloomSettings};
 
 pub struct CameraPlugin;
 
@@ -12,8 +13,31 @@ impl Plugin for CameraPlugin {
 #[derive(Component)]
 pub struct MainCamera;
 
-pub fn setup_camera(mut commands: Commands) {
-    commands.spawn(Camera2dBundle::default()).insert(MainCamera);
+#[derive(Component)]
+pub struct Background;
+
+pub fn setup_camera(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn((
+        Camera2dBundle {
+            camera: Camera {
+                hdr: true, // 1. HDR is required for bloom
+                clear_color: ClearColorConfig::Custom(Color::BLACK),
+                ..default()
+            },
+            tonemapping: Tonemapping::TonyMcMapface, // 2. Using a tonemapper that desaturates to white is recommended
+            ..default()
+        },
+        BloomSettings::default(), // 3. Enable bloom for the camera
+    )).insert(MainCamera);
+    commands.spawn(SpriteBundle {
+        texture: asset_server.load("white.png"),
+        sprite: Sprite {
+            color: Color::BLACK,
+            custom_size: Some(Vec2::splat(5000.)),
+            ..default()
+        },
+        ..default()
+    }).insert(Background);
 }
 
 pub fn close_window(keys: Res<ButtonInput<KeyCode>>, mut exit: EventWriter<AppExit>) {
